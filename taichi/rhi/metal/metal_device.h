8d2e7e225eededdd8faabef44d54db6ed68b1d39
Antonio Ferreras <antfs10@gmail.com>
2023-07-11 17:00:31 -0400

    Formatting and other fix

--- taichi/rhi/metal/metal_device.h
+++ taichi/rhi/metal/metal_device.h
@@ -1,9 +1,9 @@
 #pragma once
-#include <memory>
 #include "taichi/common/logging.h"
 #include "taichi/rhi/device.h"
-#include "taichi/rhi/metal/metal_api.h"
 #include "taichi/rhi/impl_support.h"
+#include "taichi/rhi/metal/metal_api.h"
+#include <memory>
 
 #if defined(__APPLE__) && defined(__OBJC__)
 #import <Foundation/Foundation.h>
@@ -96,20 +96,18 @@ struct MetalWorkgroupSize {
   uint32_t y{0};
   uint32_t z{0};
 };
-class MetalPipeline final : public Pipeline {
+class MetalPipeline final :  public Pipeline {
  public:
   // `mtl_library`, `mtl_function`, `mtl_compute_pipeline_state` should be
   // already retained.
-  explicit MetalPipeline(const MetalDevice &device,
-                         MTLLibrary_id mtl_library,
+  explicit MetalPipeline(const MetalDevice &device, MTLLibrary_id mtl_library,
                          MTLFunction_id mtl_function,
                          MTLComputePipelineState_id mtl_compute_pipeline_state,
                          MetalWorkgroupSize workgroup_size);
   ~MetalPipeline() final;
 
   static MetalPipeline *create(const MetalDevice &device,
-                               const uint32_t *spv_data,
-                               size_t spv_size,
+                               const uint32_t *spv_data, size_t spv_size,
                                const std::string &name);
   void destroy();
 
@@ -150,25 +148,22 @@ struct MetalShaderResource {
     MetalShaderTextureResource texture;
   };
 };
-class MetalShaderResourceSet final : public ShaderResourceSet {
+class MetalShaderResourceSet final :  public ShaderResourceSet {
  public:
   explicit MetalShaderResourceSet(const MetalDevice &device);
   ~MetalShaderResourceSet() final;
 
-  ShaderResourceSet &rw_buffer(uint32_t binding,
-                               DevicePtr ptr,
+  ShaderResourceSet &rw_buffer(uint32_t binding, DevicePtr ptr,
                                size_t size) final;
   ShaderResourceSet &rw_buffer(uint32_t binding, DeviceAllocation alloc) final;
 
   ShaderResourceSet &buffer(uint32_t binding, DevicePtr ptr, size_t size) final;
   ShaderResourceSet &buffer(uint32_t binding, DeviceAllocation alloc) final;
 
-  ShaderResourceSet &image(uint32_t binding,
-                           DeviceAllocation alloc,
+  ShaderResourceSet &image(uint32_t binding, DeviceAllocation alloc,
                            ImageSamplerConfig sampler_config) override;
 
-  ShaderResourceSet &rw_image(uint32_t binding,
-                              DeviceAllocation alloc,
+  ShaderResourceSet &rw_image(uint32_t binding, DeviceAllocation alloc,
                               int lod) override;
 
   inline const std::vector<MetalShaderResource> &resources() const {
@@ -180,7 +175,7 @@ class MetalShaderResourceSet final : public ShaderResourceSet {
   std::vector<MetalShaderResource> resources_; // TODO: need raster resources
 };
 
-class MetalCommandList final : public CommandList {
+class MetalCommandList final :  public CommandList {
  public:
   explicit MetalCommandList(const MetalDevice &device,
                             MTLCommandQueue_id cmd_queue);
@@ -198,8 +193,7 @@ class MetalCommandList final : public CommandList {
   void buffer_fill(DevicePtr ptr, size_t size, uint32_t data) noexcept final;
   RhiResult dispatch(uint32_t x, uint32_t y = 1, uint32_t z = 1) noexcept final;
 
-  void image_transition(DeviceAllocation img,
-                        ImageLayout old_layout,
+  void image_transition(DeviceAllocation img, ImageLayout old_layout,
                         ImageLayout new_layout) final;
 
   MTLCommandBuffer_id finalize();
@@ -215,7 +209,7 @@ class MetalCommandList final : public CommandList {
   const MetalShaderResourceSet *current_shader_resource_set_;
 };
 
-class MetalStream final : public Stream {
+class MetalStream final :  public Stream {
  public:
   // `mtl_command_queue` should be already retained.
   explicit MetalStream(const MetalDevice &device,
@@ -225,17 +219,15 @@ class MetalStream final : public Stream {
   static MetalStream *create(const MetalDevice &device);
   void destroy();
 
-  MTLCommandQueue_id mtl_command_queue() const {
-    return mtl_command_queue_;
-  }
+  MTLCommandQueue_id mtl_command_queue() const { return mtl_command_queue_; }
 
   RhiResult new_command_list(CommandList **out_cmdlist) noexcept final;
-  StreamSemaphore submit(
-      CommandList *cmdlist,
-      const std::vector<StreamSemaphore> &wait_semaphores = {}) final;
-  StreamSemaphore submit_synced(
-      CommandList *cmdlist,
-      const std::vector<StreamSemaphore> &wait_semaphores = {}) final;
+  StreamSemaphore
+  submit(CommandList *cmdlist,
+         const std::vector<StreamSemaphore> &wait_semaphores = {}) final;
+  StreamSemaphore
+  submit_synced(CommandList *cmdlist,
+                const std::vector<StreamSemaphore> &wait_semaphores = {}) final;
 
   void command_sync() override;
 
@@ -246,68 +238,59 @@ class MetalStream final : public Stream {
   bool is_destroyed_{false};
 };
 
-class MetalSurface final : public Surface {
-  public:
-    MetalSurface(MetalDevice *device, const SurfaceConfig &config);
-    ~MetalSurface() override;
+class MetalSurface final :  public Surface {
+ public:
+  MetalSurface(MetalDevice *device, const SurfaceConfig &config);
+  ~MetalSurface() override;
 
-    CAMetalLayer* mtl_layer() {
-      return layer_;
-    }
+  CAMetalLayer *mtl_layer() { return layer_; }
 
-    StreamSemaphore acquire_next_image() override;
-    DeviceAllocation get_target_image() override;
+  StreamSemaphore acquire_next_image() override;
+  DeviceAllocation get_target_image() override;
 
-    void present_image(
-        const std::vector<StreamSemaphore> &wait_semaphores = {}) override;
-    std::pair<uint32_t, uint32_t> get_size() override;
-    int get_image_count() override;
-    BufferFormat image_format() override;
-    void resize(uint32_t width, uint32_t height) override;
+  void present_image(
+      const std::vector<StreamSemaphore> &wait_semaphores = {}) override;
+  std::pair<uint32_t, uint32_t> get_size() override;
+  int get_image_count() override;
+  BufferFormat image_format() override;
+  void resize(uint32_t width, uint32_t height) override;
 
-    DeviceAllocation get_depth_data(DeviceAllocation &depth_alloc) override {
-      TI_NOT_IMPLEMENTED;
-    }
-    DeviceAllocation get_image_data() override {
-      TI_NOT_IMPLEMENTED;
-    }
+  DeviceAllocation get_depth_data(DeviceAllocation &depth_alloc) override {
+    TI_NOT_IMPLEMENTED;
+  }
+  DeviceAllocation get_image_data() override { TI_NOT_IMPLEMENTED; }
 
-  private:
-    void destroy_swap_chain();
+ private:
+  void destroy_swap_chain();
 
-    SurfaceConfig config_;
+  SurfaceConfig config_;
 
-    BufferFormat image_format_{BufferFormat::unknown};
+  BufferFormat image_format_{BufferFormat::unknown};
 
-    uint32_t width_{0};
-    uint32_t height_{0};
+  uint32_t width_{0};
+  uint32_t height_{0};
 
-    MTLTexture_id current_swap_chain_texture_;
-    std::unordered_map<MTLTexture_id, DeviceAllocation> swapchain_images_;
-    CAMetalDrawable_id current_drawable_;
+  MTLTexture_id current_swap_chain_texture_;
+  std::unordered_map<MTLTexture_id, DeviceAllocation> swapchain_images_;
+  CAMetalDrawable_id current_drawable_;
 
-    MetalDevice *device_{nullptr};
-    CAMetalLayer* layer_;
+  MetalDevice *device_{nullptr};
+  CAMetalLayer *layer_;
 };
 
-class MetalDevice final : public GraphicsDevice {
+class MetalDevice final :  public GraphicsDevice {
  public:
   // `mtl_device` should be already retained.
   explicit MetalDevice(MTLDevice_id mtl_device);
   ~MetalDevice() override;
 
-  Arch arch() const override {
-    return Arch::metal;
-  }
-  MTLDevice_id mtl_device() const {
-    return mtl_device_;
-  }
+  Arch arch() const override { return Arch::metal; }
+  MTLDevice_id mtl_device() const { return mtl_device_; }
 
   static MetalDevice *create();
   void destroy();
 
-  std::unique_ptr<Surface> create_surface(
-      const SurfaceConfig &config) override;
+  std::unique_ptr<Surface> create_surface(const SurfaceConfig &config) override;
 
   RhiResult allocate_memory(const AllocParams &params,
                             DeviceAllocation *out_devalloc) override;
@@ -330,22 +313,19 @@ class MetalDevice final : public GraphicsDevice {
   void unmap(DeviceAllocation ptr) override;
 
   RhiResult create_pipeline(Pipeline **out_pipeline,
-                            const PipelineSourceDesc &src,
-                            std::string name,
+                            const PipelineSourceDesc &src, std::string name,
                             PipelineCache *cache) noexcept final;
   ShaderResourceSet *create_resource_set() override;
 
-  std::unique_ptr<Pipeline> create_raster_pipeline(
-      const std::vector<PipelineSourceDesc> &src,
-      const RasterParams &raster_params,
-      const std::vector<VertexInputBinding> &vertex_inputs,
-      const std::vector<VertexInputAttribute> &vertex_attrs,
-      std::string name = "Pipeline") override {
+  std::unique_ptr<Pipeline>
+  create_raster_pipeline(const std::vector<PipelineSourceDesc> &src,
+                         const RasterParams &raster_params,
+                         const std::vector<VertexInputBinding> &vertex_inputs,
+                         const std::vector<VertexInputAttribute> &vertex_attrs,
+                         std::string name = "Pipeline") override {
     TI_NOT_IMPLEMENTED;
   }
-  RasterResources *create_raster_resources() override {
-    TI_NOT_IMPLEMENTED;
-  }
+  RasterResources *create_raster_resources() override { TI_NOT_IMPLEMENTED; }
 
   Stream *get_compute_stream() override;
   Stream *get_graphics_stream() override;
@@ -353,9 +333,7 @@ class MetalDevice final : public GraphicsDevice {
 
   void memcpy_internal(DevicePtr dst, DevicePtr src, uint64_t size) override;
 
-  const MetalSampler &get_default_sampler() const {
-    return *default_sampler_;
-  }
+  const MetalSampler &get_default_sampler() const { return *default_sampler_; }
 
  private:
   MTLDevice_id mtl_device_;
@@ -368,5 +346,5 @@ class MetalDevice final : public GraphicsDevice {
   bool is_destroyed_{false};
 };
 
-}  // namespace metal
-}  // namespace taichi::lang
+} // namespace metal
+} // namespace taichi::lang
